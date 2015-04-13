0193b1a49a2f62870e501351d4ecb5f051178171
Everton <evertonagilar@gmail.com>
2015-04-12 22:34:57 -0300

    Incluído especificação de tipos

--- src/ppca_soa.erl
+++ src/ppca_soa.erl
@@ -8,28 +8,41 @@
 
 -module(ppca_soa).
 
--export([start/1, start_listen/2, stop_listen/2]).
+-export([start/0, start/1, start_listen/2, stop_listen/2]).
+
+-include("../include/ppca_config.hrl").
 
 
+-spec start(integer()) -> pid().
 start(Port) -> 
 	io:format("PPCA_SOA - Barramento SOA da Turma PPCA 2014~n"),
 	Server = spawn(ppca_server, init, []),
-	start_listen(Server, Port),
+	case start_listen(Server, Port) of
+		ok -> io:format("Escutando na porta ~p.~n", [Port]);
+		{error, Reason} -> io:format("Não foi possível escutar na porta ~p. Motivo: ~p.~n", [Port, Reason])
+	end,
 	Server.
 
 
+-spec start() -> pid().
+start() ->
+	start(?CONF_PORT).
 
+
+-spec start_listen(Server::pid(), Port::integer()) -> ok | {error, Reason::string()}.
 start_listen(Server, Port) ->
 	Server ! { self(), {start_listen, Port}},
 	receive
-		{ Server, ok } -> io:format("Escutando na porta ~p.~n", [Port]);
-		{ Server, {error, Reason} } -> io:format("Não foi possível escutar na porta ~p.~nMotivo: ~p.~n", [Port, Reason])
+		{ Server, ok } -> ok;
+		{ Server, {error, Reason} } -> {error, Reason}
 	end.
 
 
+-spec stop_listen(Server::pid(), Port::integer()) -> ok | {error, Reason::string()}.
 stop_listen(Server, Port) ->
 	Server ! { self(), {stop_listen, Port}},
 	receive
-		{ Server, ok } -> io:format("Parou de escutar na porta ~p.~n", [Port]);
-		{ Server, {error, Reason} } -> io:format("~p.~n", [Reason])
+		{ Server, ok } -> ok;
+		{ Server, {error, Reason} } -> {errpr, Reason}
 	end.
+
