9d3ff9fdcc1b301de046e584b338bb8732b83652
Everton <evertonagilar@gmail.com>
2015-04-12 22:34:08 -0300

    Arquivo com definições de configuração

---
+++ include/ppca_config.hrl
@@ -0,0 +1,14 @@
+%% ---
+%%  PPCA_SOA
+%%  Manipula opções de configuração.
+%%  Mestrado em Computação Aplicada - Universidade de Brasília
+%%  Turma de Construção de Software / PPCA 2014
+%%  Professor: Rodrigo Bonifacio de Almeida
+%%---
+
+
+-define(CONF_PORT, 2301).
+
+
+
+
