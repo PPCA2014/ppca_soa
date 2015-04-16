00b538bccd5ad216e83a9d55012c2f17b6696c51
drausio <drausiogs@gmail.com>
2015-04-15 21:19:04 -0300

    Primeira versão

---
+++ start_server.bat
@@ -0,0 +1,11 @@
+
+
+# ---
+#  PPCA_SOA
+#  Inicia o barramento PPCA_SOA na porta 2301
+#  Mestrado em Computação Aplicada - Universidade de Brasília
+#  Turma de Construção de Software / PPCA 2014
+#  Professor: Rodrigo Bonifacio de Almeida
+# ---
+erl  -pa ..\ebin -eval "ppca_soa:start(2301)"
+	
