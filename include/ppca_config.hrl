15ae777d6f271c0963b12e68aec71c4b6c18e17e
Everton <evertonagilar@gmail.com>
2015-04-18 20:51:57 -0300

    Tabela de rotas

--- include/ppca_config.hrl
+++ include/ppca_config.hrl
@@ -10,5 +10,33 @@
 -define(CONF_PORT, 2301).
 
 
+-record(rota, {metodo="GET", url, async, funcao}).
+
+
+-define(
+    tabela_rota,
+	[ #rota{metodo="GET"
+	        url="/hello_world", 
+			async=false, 
+			funcao=fun() -> io:format("Hello world") end
+			},
+	  #rota{metodo="POST",
+			url="/hello_world", 
+			async=true, 
+			funcao=fun() -> io:format("Hello world2") end
+			}
+			
+	]
+).
+	
+
+
+
+	
+     
+     
+     	
+
+
 
 
