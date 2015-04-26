e49fe5e2d088f9d7f2292ab2009f7dc79074635d
Everton <evertonagilar@gmail.com>
2015-04-26 00:02:04 -0300

    Adaptado para behavior gen_server

--- src/ppca_event_mq.erl
+++ src/ppca_event_mq.erl
@@ -7,101 +7,155 @@
 %%---
 
 -module(ppca_event_mq).
+-behavior(gen_server).
 
--export([start/0, 
-		 adiciona_evento/1, 
+%% Server API
+-export([start/0, stop/0]).
+
+%% Client API
+-export([adiciona_evento/1, 
 		 registra_interesse/2, 
+		 desregistra_interesse/2, 
 		 lista_evento/0, 
 		 lista_interesse/0, 
-		 notifica_evento/2, 
-		 loop/2]).
+		 notifica_evento/2,
+		 cancela_evento/1]).
 
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {lista_evento = [], 
+			    lista_interesse = []}).
+
+
+%%====================================================================
+%% Server API
+%%====================================================================
 
 start() ->
-    ListaEvento = [],
-    ListaInteresse = [],
-	Pid = spawn(ppca_event_mq, loop, [ListaEvento, ListaInteresse]),
-	register(ppca_event, Pid).
-	
+    Result = gen_server:start_link({local, ?SERVER}, ?MODULE, [], []),
+    io:format("ppca_event_mq iniciado.~n"),
+    Result.
+ 
+stop() ->
+    gen_server:cast(?SERVER, shutdown).
+ 
+ 
+%%====================================================================
+%% Client API
+%%====================================================================
+ 
+adiciona_evento(Evento) -> 
+	gen_server:call(?SERVER, {adiciona_evento, Evento}). 
+cancela_evento(Evento) -> 
+	gen_server:call(?SERVER, {cancela_evento, Evento}). 
+registra_interesse(Evento, Fun) -> 
+	gen_server:call(?SERVER, {registra_interesse, Evento, Fun}). 
+desregistra_interesse(Evento, Fun) -> 
+	gen_server:call(?SERVER, {desregistra_interesse, Evento, Fun}). 
+notifica_evento(QualEvento, Motivo) -> 
+	gen_server:call(?SERVER, {notifica_evento, QualEvento, Motivo}). 
+lista_evento() -> 
+	gen_server:call(?MODULE, msg_lista_evento). 
+lista_interesse() -> 
+	gen_server:call(?SERVER, lista_interesse). 
 
-adiciona_evento(Evento) ->
-	ppca_event ! {self(), {adiciona_evento, Evento}},
-	receive
-		Msg -> Msg
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+ 
+init([]) ->
+    {ok, #state{}}.
+    
+handle_cast(shutdown, State) ->
+    {stop, normal, State}.
+    
+handle_call({adiciona_evento, Evento}, _From, State) ->
+	{Reply, NewState} = novo_evento(Evento, State),
+	{reply, Reply, NewState};
+
+handle_call({cancela_evento, Evento}, _From, State) ->
+	{Reply, NewState} = cancela_evento(Evento, State),
+	{reply, Reply, NewState};
+
+handle_call({registra_interesse, Evento, Fun}, _From, State) ->
+	{Reply, NewState} = novo_interesse(Evento, Fun, State),
+	{reply, Reply, NewState};
+
+handle_call({desregistra_interesse, Evento, Fun}, _From, State) ->
+	{Reply, NewState} = remove_interesse(Evento, Fun, State),
+	{reply, Reply, NewState};
+    
+handle_call({notifica_evento, QualEvento, Motivo}, _From, State) ->
+	Reply = notifica_evento(QualEvento, Motivo, State),
+	{reply, Reply, State};
+    
+handle_call(msg_lista_evento, _From, State) ->
+	Reply = lista_evento(State),
+	{reply, Reply, State};
+
+handle_call(lista_interesse, _From, State) ->
+	Reply = lista_interesse(State),
+	{reply, Reply, State}.
+    
+handle_info(_Info, State) ->
+    {noreply, State}.
+ 
+terminate(_Reason, _State) ->
+    io:format("ppca_event_mq finalizado.~n"),
+    ok.
+ 
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+    
+    
+%%====================================================================
+%% Funções internas
+%%====================================================================
+    
+existe_evento(Evento, State) ->
+	lists:member(Evento, State#state.lista_evento).
+
+existe_interesse(Interesse, State) ->
+	lists:member(Interesse, State#state.lista_interesse).
+    
+novo_evento(Evento, State) ->
+	case existe_evento(Evento, State) of
+		true  -> {eeventojacadastrado, State};
+		false -> {ok, State#state{lista_evento=[Evento|State#state.lista_evento]}}
 	end.
-	
 
-registra_interesse(Evento, Fun) ->
-	ppca_event ! {self(), {registra_interesse, {Evento, Fun}}},
-	receive
-		ok -> ok;
-		{error, Reason} -> Reason
-	end.
+cancela_evento(Evento, State) ->
+	{ok, State#state{lista_evento = State#state.lista_evento -- [Evento], 
+		  			 lista_interesse = [I || {E,_} = I <- State#state.lista_interesse, E /= Evento]}}.
 
-
-lista_evento() ->
-	ppca_event ! {self(), lista_evento},
-	receive
-		Msg -> Msg
-	end.
-
-
-lista_interesse() ->
-	ppca_event ! {self(), lista_interesse},
-	receive
-		Msg -> Msg
-	end.
-
-
-notifica_evento(QualEvento, Motivo) ->
-	ppca_event ! {self(), {notifica_evento, QualEvento, Motivo}},
-	receive
-		ok -> ok;
-		{error, Reason} -> Reason
-	end.
-	
-
-loop(ListaEvento, ListaInteresse) ->
-	receive
-		{From, {adiciona_evento, Evento}} ->
-			case lists:member(Evento, ListaEvento) of
-				true -> 
-					From ! ok,
-					loop(ListaEvento, ListaInteresse);
-				false -> 
-					From ! ok,
-					loop([Evento|ListaEvento], ListaInteresse)
+novo_interesse(Evento, Fun, State) ->
+	case existe_evento(Evento, State) of
+		true -> 
+			case existe_interesse({Evento, Fun}, State) of
+				true  -> {einteressejacadastrado, State};
+				false -> {ok, State#state{lista_interesse=[{Evento, Fun}|State#state.lista_interesse]}}
 			end;
-		{From, {notifica_evento, QualEvento, Motivo}} ->
-			case lists:member(QualEvento, ListaEvento) of
-				true -> 
-					[Fun(QualEvento, Motivo) || {Interesse, Fun} <- ListaInteresse, Interesse == QualEvento],
-					From ! ok;
-				false -> 
-					From ! {error, enotsubscribe}
-			end,
-			loop(ListaEvento, ListaInteresse);
-		{From, {registra_interesse, {Evento, Fun}}} ->
-			case lists:member(Evento, ListaEvento) of
-				true -> 
-					case lists:member({Evento, Fun}, ListaInteresse) of
-						true -> 
-							From ! ok,
-							loop(ListaEvento, ListaInteresse);
-						false -> 
-							From ! ok,
-							loop(ListaEvento, [{Evento, Fun}|ListaInteresse])
-					end;
-				false -> 
-					From ! {error, eeventnotexist},
-					loop(ListaEvento, ListaInteresse)
-			end;
-		{From, lista_evento} ->
-			From ! ListaEvento,
-			loop(ListaEvento, ListaInteresse);
-		{From, lista_interesse} ->
-			From ! ListaInteresse,
-			loop(ListaEvento, ListaInteresse)
+		false -> {eeventonaocadastrado, State}
 	end.
-	
 
+remove_interesse(Evento, Fun, State) ->
+	case existe_interesse({Evento, Fun}, State) of
+		true  -> {ok, State#state{lista_interesse = State#state.lista_interesse -- [{Evento, Fun}]}};
+		false -> {einteressenaocadastrado, State}
+	end.
+		
+notifica_evento(QualEvento, Motivo, State) ->
+	case existe_evento(QualEvento, State) of
+		true  -> 
+			[Fun(QualEvento, Motivo) || {Evento, Fun} <- State#state.lista_interesse, Evento == QualEvento],
+			ok;
+		false -> eeventonaocadastrado
+	end.
+
+lista_evento(State) -> State#state.lista_evento.	
+
+lista_interesse(State) -> State#state.lista_interesse.	
