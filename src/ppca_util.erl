051d9ed187c52ed4638a5193f8ac6d23b165278c
unknown <69493839168@CPD-DES-374410.ad-fub.unb.br>
2015-04-29 13:49:15 -0300

    Funções para encode/decode JSON

--- src/ppca_util.erl
+++ src/ppca_util.erl
@@ -7,7 +7,10 @@
 %%---
 -module(ppca_util).
 
--export([sleep/1]).
+-export([sleep/1,
+		 timestamp_str/0,
+		 encode_json/1,
+		 decode_json/1]).
 
 sleep(T) ->
     receive
@@ -16,5 +19,17 @@ sleep(T) ->
     end.
 
 
+timestamp_str() ->
+	{{Ano,Mes,Dia},{Hora,Min,Seg}} = calendar:local_time(),
+	lists:flatten(io_lib:format("~p/~p/~p ~p:~p:~p", [Dia, Mes, Ano, Hora, Min, Seg])).
+	%%"30/03/2015 11:43:00".
+	
+	
+encode_json(Json)->
+	jiffy:encode(Json).
+
+	
+decode_json(IoData) ->
+	jiffy:decode(IoData).
 
 
