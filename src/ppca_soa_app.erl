a7dbfbc1dbf64531d573fa2d27519aa5b36da681
Everton <evertonagilar@gmail.com>
2015-04-29 20:45:10 -0300

    Arquivos da arquitetura OTP

---
+++ src/ppca_soa_app.erl
@@ -0,0 +1,16 @@
+-module(ppca_soa_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ppca_soa_sup:start_link().
+
+stop(_State) ->
+    ok.
