1f13d9ecacc06777d1be659deff3469a8d656e23
Everton <evertonagilar@gmail.com>
2015-05-03 02:17:22 -0300

    Ajustes gerais

--- src/ppca_util.erl
+++ src/ppca_util.erl
@@ -23,11 +23,13 @@ timestamp_str() ->
 	{{Ano,Mes,Dia},{Hora,Min,Seg}} = calendar:local_time(),
 	lists:flatten(io_lib:format("~p/~p/~p ~p:~p:~p", [Dia, Mes, Ano, Hora, Min, Seg])).
 	
-json_encode(Json)->
-	jiffy:encode(Json).
+json_encode(_Json)->
+	%jiffy:encode(Json).
+	"".
 	
-json_decode(IoData) ->
-	jiffy:decode(IoData).
+json_decode(_IoData) ->
+	%jiffy:decode(IoData).
+	"".
 
 hd_or_empty(List) when length(List) > 0 -> 
 	hd(List);
