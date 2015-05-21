e45cb98a4ce37be64f6d6e4b6b8366cf950c48ec
Everton <evertonagilar@gmail.com>
2015-05-20 22:08:42 -0300

    Implementado json_decode_as_map(JSON)

--- src/ppca_util.erl
+++ src/ppca_util.erl
@@ -12,34 +12,47 @@
 		 timestamp_str/0,
 		 json_encode/1,
 		 json_decode/1,
-		 hd_or_empty/1]).
+		 hd_or_empty/1,
+		 json_decode_as_map/1]).
 
+%% @doc Dorme por um determinado tempo
 sleep(T) ->
     receive
 	    after T ->
 	       true
     end.
 
+%% @doc Retorna o timestamp em formato texto
 timestamp_str() ->
 	{{Ano,Mes,Dia},{Hora,Min,Seg}} = calendar:local_time(),
 	lists:flatten(io_lib:format("~p/~p/~p ~p:~p:~p", [Dia, Mes, Ano, Hora, Min, Seg])).
-	
+
+%% @doc Converte dados Erlang para JSON
 json_encode(JSON)->
 	jsx:encode(JSON).
-	
+
+%% @doc Converte um JSON para dados Erlang usando map
+json_decode_as_map(JSON) ->
+	try
+		Result = jsx:decode(JSON, [return_maps]),
+		{ok, Result}
+	catch
+		_Exception:Reason -> {error, Reason}
+	end.
+
+%% @doc Converte um JSON para dados Erlang
 json_decode(JSON) ->
 	try
 		Result = jsx:decode(JSON),
 		{ok, Result}
 	catch
-		_Exception:_Reason -> 
-			_Reason,
-			
-			{error, einvalid_json}
+		_Exception:Reason -> {error, Reason}
 	end.
-
+	
+%% @doc Retorna o primeiro item da lista ou vazio
 hd_or_empty(List) when length(List) > 0 -> 
 	hd(List);
-	
+
+%% @doc Retorna o primeiro item da lista ou vazio	
 hd_or_empty(_) -> [].
 
