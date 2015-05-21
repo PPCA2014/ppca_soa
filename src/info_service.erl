b40591a3ce28c4888f358dd349959aefc3acd7e0
Everton <evertonagilar@gmail.com>
2015-05-20 22:18:42 -0300

    Serviço para info

---
+++ src/info_service.erl
@@ -0,0 +1,93 @@
+%% ---
+%%  info_service
+%%  Mestrado em Computação Aplicada - Universidade de Brasília
+%%  Turma de Construção de Software / PPCA 2014
+%%  Professor: Rodrigo Bonifacio de Almeida
+%%  Aluno: Everton de Vargas Agilar (evertonagilar@gmail.com)
+%%---
+
+-module(info_service).
+
+-behavior(gen_server). 
+
+-include("../include/ppca_config.hrl").
+
+%% Server API
+-export([start/0, stop/0]).
+
+%% Cliente interno API
+-export([execute/2]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/1, handle_info/2, terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+%  Armazena o estado do servico. 
+-record(state, {}). 
+
+
+%%====================================================================
+%% Server API
+%%====================================================================
+
+start() -> 
+    Result = gen_server:start_link({local, ?SERVER}, ?MODULE, [], []),
+    io:format("info_service iniciado.~n", []),
+    Result.
+ 
+stop() ->
+    gen_server:cast(?SERVER, shutdown).
+ 
+ 
+%%====================================================================
+%% Cliente API
+%%====================================================================
+ 
+execute(HeaderDict, From)	->
+	gen_server:cast(?SERVER, {info, HeaderDict, From}).
+	
+
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+ 
+init([]) ->
+    {ok, #state{}}. 
+    
+handle_cast(shutdown, State) ->
+    {stop, normal, State};
+
+handle_cast({info, HeaderDict, From}, State) ->
+	{Response, NewState} = do_info(HeaderDict, State),
+	From ! {ok, Response}, 
+	{noreply, NewState}.
+    
+handle_call({info, HeaderDict}, _From, State) ->
+	{Response, NewState} = do_info(HeaderDict, State),
+	{reply, Response, NewState}.
+
+handle_info(State) ->
+   {noreply, State}.
+
+handle_info(_Msg, State) ->
+   {noreply, State}.
+
+terminate(_Reason, _State) ->
+    io:format("info_service finalizado.~n"),
+    ok.
+ 
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+    
+    
+%%====================================================================
+%% Funções internas
+%%====================================================================
+    
+do_info(_HeaderDict, State) ->
+	Response = "{\"Its works!!!\"}",
+	NewState = State#state{},
+	{Response, NewState}.
+
