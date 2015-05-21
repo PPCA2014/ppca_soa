d37026635b9c969e2c8557dde862e0ceebede04e
Everton <evertonagilar@gmail.com>
2015-05-20 22:19:46 -0300

    Registro do router

--- src/ppca_request.erl
+++ src/ppca_request.erl
@@ -1,15 +1,18 @@
 %% ---
 %%  PPCA_SOA
-%%  Tratador de requisições
+%%  Publish and subscribe message queue
 %%  Mestrado em Computação Aplicada - Universidade de Brasília
 %%  Turma de Construção de Software / PPCA 2014
 %%  Professor: Rodrigo Bonifacio de Almeida
+%%  Alunos: Everton de Vargas Agilar (evertonagilar@gmail.com)
+%%          Drausio
 %%---
 -module(ppca_request).
 
 -export([init/0]).
 
 init() ->
+	register(ppca_route, spawn(fun() -> ppca_route:init() end)),
 	loop().
 
 
@@ -27,38 +30,38 @@ loop() ->
 	
 	
 processa_servico(From, HeaderDict, Payload) ->
-	
-		Metodo = dict:fetch("Metodo", HeaderDict),
-	    Url = dict:fetch("Url", HeaderDict),
-		ServManager = spawn(ppca_route, init, []),
-		ServManager ! {self(), { Metodo, Url, Payload,HeaderDict}},
-			
-		receive
-		{From1,{route_deleted,ok,UrlToRemove,From}}  -> 
-				Response = "servi�o | " ++ UrlToRemove ++" | excluido~n",
+	Metodo = dict:fetch("Metodo", HeaderDict),
+	Url = dict:fetch("Url", HeaderDict),
+	ppca_route ! {self(), {Metodo, Url, Payload,HeaderDict}},
+	receive
+		{_From1, {route_deleted, ok, UrlToRemove, From}}  -> 
+				Response = "serviço | " ++ UrlToRemove ++" | excluido~n",
 				From ! { ok, Response};
 			
-		{From1,{route_added,ok,From} } -> 
-				Response = "servi�o adicionado~n",
+		{_From1, {route_added, ok, From} } -> 
+				Response = "serviço adicionado~n",
 				From ! { ok, Response};
 			
-		{From1,{service_found,HeaderDict,Target}}  -> 
+		{_From1, {service_found, HeaderDict, Target}}  -> 
 				if  Target == [] ->
-            		Response = "Not found" ,
+					Response = "Not found" ,
 					From ! { ok, Response};
-        		true -> 
-            		Module = lists:nth(1,string:tokens(Target, ":")),
+				true -> 
+					Module = lists:nth(1,string:tokens(Target, ":")),
 					Function = lists:nth(2,string:tokens(Target, ":")),
-					PidModulo = spawn(list_to_atom(Module), list_to_atom(Function), [HeaderDict,From])	
+					
+					% Apenas envia mensagem para o serviço (não cria outro serviço)
+					apply(list_to_atom(Module), list_to_atom(Function), [HeaderDict,From])
+					
+					% Cria outro serviço e envia mensagem
+					%PidModulo = spawn(list_to_atom(Module), list_to_atom(Function), [HeaderDict,From])	
 				end;
-			    
-		{From1,{route_updated,Url,ModuleFunction,From}}  -> 
+				
+		{_From1, {route_updated, Url, ModuleFunction, From}}  -> 
 				Response = "teste passou - rota "++ " atualizada " "para " ++ ModuleFunction ++"~n",
 				From ! { ok, Response};
 			
-		{From1,{route_normal,Method,Url}}  -> 
+		{_From1, {route_normal, _Method, Url}}  -> 
 				Response = "Hello" ,
 				From ! { ok, Response}
-		
-		
-		end.
+	end.
