19034f3f5ad3395738f14fae9e883aa0440da6f7
Everton <evertonagilar@gmail.com>
2015-05-23 22:49:18 -0300

    Arquivo com constantes de erro HTTP

---
+++ include/http_messages.hrl
@@ -0,0 +1,19 @@
+%% ---
+%%  PPCA_SOA
+%%  Definição das mensagens HTTP de retorno
+%%  Mestrado em Computação Aplicada - Universidade de Brasília
+%%  Turma de Construção de Software / PPCA 2014
+%%  Alunos: Everton de Vargas Agilar (evertonagilar@gmail.com)
+%%---
+
+%% Mensagens de erro 
+-define(MSG_SERVICO_NAO_ENCONTRADO, "Serviço ~p não encontrado.").
+-define(MSG_SERVICO_NAO_DISP, "Serviço ~p listado no catálogo mas não disponível.").
+-define(MSG_SERVICO_FALHOU, "Serviço ~p falhou ao atender solicitação. Motivo: ~p.").
+
+
+%% Mensagens de erro HTTP no formato JSON
+-define(HTTP_ERROR_415, <<"{\"error\":\"HTTP 415\",\"message\":\"O servidor aceita somente JSON.\"}">>).
+-define(HTTP_ERROR_404, <<"{\"error\":\"HTTP 404\",\"message\":\"Serviço não encontrado.\"}">>).
+-define(HTTP_ERROR_502(Motivo), io_lib:format(<<"{\"error\":\"HTTP 502\",\"message\":\"Serviço falhou ao atender solicitação. Motivo: ~p.\"}">>, [Motivo])).
+-define(HTTP_ERROR_503, <<"{\"error\":\"HTTP 503\",\"message\":\"Serviço listado no catálogo de serviço mas não disponível.\"}">>).
