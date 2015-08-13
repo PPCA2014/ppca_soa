cdcf0295b624b609b8caeabe11cdf91327098e43
Everton <evertonagilar@gmail.com>
2015-08-13 01:44:16 -0300

    Pequenos ajustes

--- start_daemon.sh
+++ start_daemon.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-erl -detached  -pa ../msbus/ebin deps/jsx/ebin -eval "application:start(msbus)" -boot start_sasl -config ./priv/conf/elog
+erl -detached  -pa ../msbus/ebin deps/jsx/ebin deps/poolboy/ebin -eval "application:start(msbus)" -boot start_sasl -config ./priv/conf/elog
 	
