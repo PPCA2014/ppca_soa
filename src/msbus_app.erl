eb40dc93605eaaa4bb7f4813c24687b7ae33cdca
Everton <evertonagilar@gmail.com>
2015-08-30 00:30:48 -0300

    Implementado arquivo configuração

--- src/msbus_app.erl
+++ src/msbus_app.erl
@@ -21,7 +21,6 @@ start(_StartType, StartArgs) ->
     T1 = msbus_util:get_milliseconds(),
     io:format("~s~n", [?SERVER_NAME]),
     Ret = msbus_sup:start_link(StartArgs),
-    Conf = msbus_config:getConfig(),
     io:format("Portal ErlangMS Api Management em http://127.0.0.1:~p/portal/index.html~n", [(msbus_config:getConfig())#config.tcp_port]),
     io:format("ErlangMS iniciado em ~pms.~n", [msbus_util:get_milliseconds() - T1]),
     Ret.
