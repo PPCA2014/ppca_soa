75be537ba1492c1f45b9d130ab92e0c6c5591096
Everton <evertonagilar@gmail.com>
2015-09-07 17:22:14 -0300

    Pequenos ajustes

--- src/msbus_health.erl
+++ src/msbus_health.erl
@@ -132,7 +132,11 @@ get_requests_periodo_by_group(Periodo, FieldsGroup, State) ->
 
 %% @doc Retorna os serviços mais acessados de um período
 do_get_top_services(Top, Periodo, Sort, State) ->
-    Fields = fun(X) -> {X#request.servico#servico.name} end,
+    Fields = fun(X) -> case X#request.servico of
+						  undefined -> {};
+						  _ -> {X#request.servico#servico.name} 
+					   end
+			 end,
 	Requests = get_requests_periodo(Periodo, State), 
 	Urls = maps:keys(groupBy(Fields, Requests)),
 	Urls2 = count(Fields, Urls, Requests),
