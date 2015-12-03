e390a7c9050c28cf83c24617063bb982edff29dc
Everton <evertonagilar@gmail.com>
2015-12-03 20:07:31 -0200

    Pequenos ajustes

--- start.bat
+++ start.bat
@@ -1,7 +1,6 @@
+@echo off
 REM
 REM start msbus and build before if necessary
 REM author Everton de Vargas Agilar <<evertonagilar@gmail.com>>
 REm
-@echo off
-./build.bat
 erl -pa ..\msbus\ebin deps\jsx\ebin deps\poolboy\ebin -sname msbus -setcookie erlangms -eval "msbus:start()" -boot start_sasl -config .\priv\conf\elog
