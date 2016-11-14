e93800df9a9a2962ad0017cb7c9e0a53c012d8d9
evertonagilar <evertonagilar@gmail.com>
2016-11-14 16:00:42 -0200

    pequenos ajustes

--- src/api_query/ems_api_query_service.erl
+++ src/api_query/ems_api_query_service.erl
@@ -8,124 +8,55 @@
 
 -module(ems_api_query_service).
 
--behavior(gen_server). 
--behaviour(poolboy_worker).
-
--compile(export_all).
-
 -include("../../include/ems_config.hrl").
 -include("../../include/ems_schema.hrl").
 
 
-%% Server API
--export([start/1, start_link/1, stop/0]).
-
 %% Client API
--export([find/2, find_by_id/2, insert/2, update/2]).
+-export([find/1, find_by_id/1, insert/1, update/1]).
 
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/1, handle_info/2, terminate/2, code_change/3]).
-
--define(SERVER, ?MODULE).
-
-%  Stores the state of the service.
--record(state, {}).  
-
-
-%%====================================================================
-%% Server API
-%%====================================================================
-
-start(_) -> 
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
- 
-start_link(Args) ->
-    gen_server:start_link(?MODULE, Args, []).
-
-stop() ->
-    gen_server:cast(?SERVER, shutdown).
- 
  
 %%====================================================================
 %% Client API
 %%====================================================================
  
-find(Request, From) ->
-	ems_pool:cast(ems_api_query_service, {find, Request, From}).
+find(Request) -> 
+	Result = execute_command(find, Request),
+	{ok, Request#request{code = 200,
+						 response_data = ems_schema:to_json(Result)}}.
 
-find_by_id(Request, From) ->
-	ems_pool:cast(ems_api_query_service, {find_by_id, Request, From}).
 
-insert(Request, From) ->
-	ems_pool:cast(ems_api_query_service, {insert, Request, From}).
+find_by_id(Request) ->
+	Result = execute_command(find_by_id, Request),
+	{ok, Request#request{code = 200,
+						 response_data = ems_schema:to_json(Result)}}.
 
-update(Request, From) ->
-	ems_pool:cast(ems_api_query_service, {update, Request, From}).
+insert(Request) ->
+	Result = execute_command(insert, Request),
+	{ok, Request#request{code = 200,
+						 response_data = ems_schema:to_json(Result)}}.
 
-%%====================================================================
-%% gen_server callbacks
-%%====================================================================
- 
-init(_Args) ->
-    process_flag(trap_exit, true),
-    State = #state{},
-    {ok, State}. 
-    
-    
-handle_cast(shutdown, State) ->
-    {stop, normal, State};
+update(Request) ->
+	Result = execute_command(update, Request),
+	{ok, Request#request{code = 200,
+						 response_data = ems_schema:to_json(Result)}}.
 
-handle_cast({find, Request, _From}, State) ->
-	Result = execute_command(find, Request, State),
-	ems_eventmgr:notifica_evento(ok_request, {service, Request, Result}),
-	{noreply, State};
-
-handle_cast({find_by_id, Request, _From}, State) ->
-	Result = execute_command(find_by_id, Request, State),
-	ems_eventmgr:notifica_evento(ok_request, {service, Request, Result}),
-	{noreply, State};
-
-handle_cast({update, Request, _From}, State) ->
-	Result = execute_command(update, Request, State),
-	ems_eventmgr:notifica_evento(ok_request, {service, Request, Result}),
-	{noreply, State};
-
-handle_cast({insert, Request, _From}, State) ->
-	Result = execute_command(insert, Request, State),
-	ems_eventmgr:notifica_evento(ok_request, {service, Request, Result}),
-	{noreply, State}.
-
-handle_call(_Msg, _From, State) ->
-	{reply, ok, State}.
-
-handle_info(State) ->
-   {noreply, State}.
-
-handle_info(_Msg, State) ->
-   {noreply, State}.
-
-terminate(_Reason, _State) ->
-    ok.
- 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-    
+  
     
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
 
-
-execute_command(Command, Request = #request{service = #service{datasource = Datasource}}, State) ->
+execute_command(Command, Request = #request{service = #service{datasource = Datasource}}) ->
 	try
 		case ems_db:get_connection(Datasource) of
 			{ok, Datasource2} -> 
 				Result = case Command of
-					find -> do_find(Request, Datasource2, State);
-					find_by_id -> do_find_by_id(Request, Datasource2, State);
-					insert -> do_insert(Request, Datasource2, State);
-					update -> do_update(Request, Datasource2, State)
+					find -> do_find(Request, Datasource2);
+					find_by_id -> do_find_by_id(Request, Datasource2);
+					insert -> do_insert(Request, Datasource2);
+					update -> do_update(Request, Datasource2)
 				end,
 				ems_db:release_connection(Datasource2),
 				Result;
@@ -138,7 +69,7 @@ execute_command(Command, Request = #request{service = #service{datasource = Data
 
 do_find(#request{querystring_map = QuerystringMap, 
 				 service = #service{debug = Debug}},
-				 Datasource, _State) ->
+				 Datasource) ->
 	FilterJson = maps:get(<<"filter">>, QuerystringMap, <<>>),
 	Fields = binary_to_list(maps:get(<<"fields">>, QuerystringMap, <<>>)),
 	Limit = binary_to_integer(maps:get(<<"limit">>, QuerystringMap, <<"100">>)),
@@ -149,17 +80,17 @@ do_find(#request{querystring_map = QuerystringMap,
 
 do_find_by_id(Request = #request{querystring_map = QuerystringMap, 
 								 service = #service{debug = Debug}}, 
-			 Datasource, _State) ->
+			 Datasource) ->
 	Id = ems_request:get_param_url(<<"id">>, 0, Request),
 	Fields = binary_to_list(maps:get(<<"fields">>, QuerystringMap, <<>>)),
 	ems_api_query:find_by_id(Id, Fields, Datasource, Debug).
 
 
-do_insert(#request{payload_map = Payload, service = Service}, Datasource, _State) ->
+do_insert(#request{payload_map = Payload, service = Service}, Datasource) ->
 	ems_api_query:insert(Payload, Service, Datasource).
 
 
-do_update(Request = #request{payload_map = Payload, service = Service}, Datasource, _State) ->
+do_update(Request = #request{payload_map = Payload, service = Service}, Datasource) ->
 	Id = ems_request:get_param_url(<<"id">>, 0, Request),
 	ems_api_query:update(Id, Payload, Service, Datasource).
 
