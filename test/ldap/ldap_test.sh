3a4559aba65943442a61553c2a38268b7c470e71
evertonagilar <evertonagilar@gmail.com>
2016-11-28 14:43:25 -0200

    pequenos ajustes

--- test/ldap/ldap_test.sh
+++ test/ldap/ldap_test.sh
@@ -4,18 +4,46 @@
 # Author: Everton de Vargas Agilar
 # Data: 16/03/2016
 #
+# Modo de uso:
+#   ./ldap_test.sh qtd_requests host_ldap
+#       qtd_requests => número de requisições simultâneas (default é 100)
+#       host_ldap    => host do ldap (default é localhost)
+#
+#
 
-if [ $1 == "" ]; then
-	echo "Uso: ldap_test.sh count_reqs host"
-else
-	 COUNTER=$1
-	 echo "$COUNTER asynchronous ldap requests to the ErlangMS..."
-	 until [  $COUNTER -lt 0 ]; do
-		 let COUNTER-=1
-		 ldapsearch -xLLL -h $2:2389 -b 'dc=unb,dc=br' -D 'cn=admin,dc=unb,dc=br' uid=geral -w 123456 
-	 done
-	 echo "done!"
+echo "Script de teste para o módulo LDAP server do barramento"
+echo "Date: $(date '+%d/%m/%Y %H:%M:%S')"
+	
+# Verifica se não há parâmetros
+if [ $# = 0 ] || [ "$1" = "--help" ]; then
+	echo "Modo de usar: ./ldap_test.sh qtd_requests host_ldap"
+	echo "         qtd_requests => número de requisições simultâneas (default é 100)"
+	echo "         host_ldap    => host do ldap (default é localhost:2389)"	
+	exit
 fi
+
+# Parâmetro qtd_requests
+if [ "$1" == "" ]; then
+	COUNTER=100
+else
+	COUNTER=$1
+fi
+
+# Parâmetro host_ldap
+if [ "$2" == "" ]; then
+       HOST_LDAP="localhost:2389"
+else
+       HOST_LDAP="$2"
+fi
+
+echo ""
+echo "Realizando $COUNTER requisições ldap para o barramento..."
+echo ""
+until [  $COUNTER -lt 1 ]; do
+	let COUNTER-=1
+	ldapsearch -xLLL -h "$HOST_LDAP" -b 'dc=unb,dc=br' -D 'cn=admin,dc=unb,dc=br' uid=geral -w 123456 >> ldap_test.log &
+done
+
 
 
 			         
