11f9a671aef257996358b5d76faace42c85e68c0
evertonagilar <evertonagilar@gmail.com>
2017-01-06 11:12:39 -0200

    pequenos ajustes

--- test/ldap/ldap_test.sh
+++ test/ldap/ldap_test.sh
@@ -11,20 +11,18 @@
 #
 #
 
-echo "Script de teste para o módulo LDAP server do barramento"
-echo "Date: $(date '+%d/%m/%Y %H:%M:%S')"
-	
 # Verifica se não há parâmetros
-if [ $# = 0 ] || [ "$1" = "--help" ]; then
-	echo "Modo de usar: ./ldap_test.sh qtd_requests host_ldap"
+if [ $# != 3 ] || [ "$1" = "--help" ]; then
+	echo "Modo de usar: ./ldap_test.sh qtd_requests host_ldap user"
 	echo "         qtd_requests => número de requisições simultâneas (default é 100)"
 	echo "         host_ldap    => host do ldap (default é localhost:2389)"	
+	echo "         user         => user da pesquisa"	
 	exit
 fi
 
 # Parâmetro qtd_requests
 if [ "$1" == "" ]; then
-	COUNTER=100
+	COUNTER=1
 else
 	COUNTER=$1
 fi
@@ -36,13 +34,30 @@ else
        HOST_LDAP="$2"
 fi
 
-echo ""
-echo "Realizando $COUNTER requisições ldap para o barramento..."
-echo ""
-until [  $COUNTER -lt 1 ]; do
-	let COUNTER-=1
-	ldapsearch -xLLL -h "$HOST_LDAP" -b 'dc=unb,dc=br' -D 'cn=admin,dc=unb,dc=br' uid=geral -w 123456 >> ldap_test.log &
-done
+# Parâmetro user
+if [ "$3" == "" ]; then
+       USER="geral"
+else
+       USER="$3"
+fi
 
+echo "Informe a senha do usuário para autenticação:"
+read -s USER_PASSWD
 
+if [ $COUNTER == 1 ]; then
+	echo ""
+	echo "Realizando requisição ldap para o barramento com user $USER..."
+	echo ldapsearch -xLLL -h "$HOST_LDAP" -b 'dc=unb,dc=br' -D 'cn=admin,dc=unb,dc=br' uid=$USER
+	echo ""
+	ldapsearch -xLLL -h "$HOST_LDAP" -b 'dc=unb,dc=br' -D 'cn=admin,dc=unb,dc=br' uid=$USER -w $USER_PASSWD
+else
+	echo ""
+	echo "Realizando $COUNTER requisições ldap para o barramento com user $USER..."
+	echo ldapsearch -xLLL -h "$HOST_LDAP" -b 'dc=unb,dc=br' -D 'cn=admin,dc=unb,dc=br' uid=$USER
+	echo ""
+	until [  $COUNTER -lt 1 ]; do
+		let COUNTER-=1
+		ldapsearch -xLLL -h "$HOST_LDAP" -b 'dc=unb,dc=br' -D 'cn=admin,dc=unb,dc=br' uid=$USER -w 123456 >> ldap_test.log &
+	done
+fi
 			         
