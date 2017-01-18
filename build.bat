63559420549d9a613db517f60efc5ed06c579cc0
evertonagilar <evertonagilar@gmail.com>
2017-01-18 21:56:37 -0200

    improve start.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
+rmdir /S /Q priv\log
+rmdir /S /Q priv\db
 tools/rebar/rebar get-deps clean compile
--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 REM
 REM author Everton de Vargas Agilar <<evertonagilar@gmail.com>>
 REm
-erl -pa .\ebin deps\jiffy\ebin deps\jesse\ebin deps\jsx\ebin deps\poolboy\ebin deps\cowboy\ebin deps\cowlib\ebin deps\erlydtl\ebin deps\jesse\ebin deps\json_rec\ebin deps\mochiweb\ebin deps\oauth2\ebin deps\parse_trans\ebin deps\ranch\ebin -sname emsbus -setcookie erlangms -eval "ems_bus:start()" -boot start_sasl -config .\priv\conf\elog
+erl -pa .\ebin deps\jiffy\ebin deps\jesse\ebin deps\jsx\ebin deps\poolboy\ebin deps\cowboy\ebin deps\cowlib\ebin deps\erlydtl\ebin deps\json_rec\ebin deps\mochiweb\ebin deps\oauth2\ebin deps\parse_trans\ebin deps\ranch\ebin -sname emsbus -setcookie erlangms -eval "ems_bus:start()" -boot start_sasl -config .\priv\conf\elog
 
 
 		
