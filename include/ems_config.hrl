c5491442be72d80d8e534c724ea8ac522dc925a2
evertonagilar <evertonagilar@gmail.com>
2017-02-02 15:26:01 -0200

    improve ldap server

--- include/ems_config.hrl
+++ include/ems_config.hrl
@@ -7,8 +7,8 @@
 %%********************************************************************
 
 -ifdef(debug).
-	-define(DEBUG(Msg), io:format("\033[0;34mDEBUG ~p\033[0m\n", [Msg])).
-	-define(DEBUG(Msg, Params), io:format( "\033[0;34mDEBUG " ++ Msg ++ "\033[0m\n", Params)).
+	-define(DEBUG(Msg), io:format("\033[0;34mDEBUG ~s\033[0m\n", [Msg])).
+	-define(DEBUG(Msg, Params), io:format("\033[0;34mDEBUG " ++ Msg ++ "\033[0m\n", Params)).
 -else.
 	-define(DEBUG(Msg), ok).
 	-define(DEBUG(Msg, Params), ok).
