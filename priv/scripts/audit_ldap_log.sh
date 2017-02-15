9ebc2655cc7fab40940fd066bdcdd57443934b48
evertonagilar <evertonagilar@gmail.com>
2017-02-15 21:37:02 -0200

    improve ldap log

--- priv/scripts/audit_ldap_log.sh
+++ priv/scripts/audit_ldap_log.sh
@@ -39,61 +39,6 @@ ENVIRONMENT="desenvolvimento"
 MMIN="1440"
 CURRENT_DATE=$(date '+%d/%m/%Y %H:%M:%S')
 
-echo "Starting audit_ldap_log.sh Version: $VERSION - ERLANGMS"
-
-
-# Check parameters
-if [ "$#" = "0" ] && [ "$1" = "--help" ]; then
-	echo "How to use: ./audit_ldap_log.sh minutes"
-	echo "where minutes is logfile's data was last modified minutes ago (default is 43200 minutes)"
-elif [ "$#" = "1" ]; then
-	MMIN="$1"
-	RE='^[0-9]{1,4}$'
-	if ! [[ $MMIN =~ $RE ]] ; then
-		echo "Parameter minutes with value \"$MMIN\" is inválid. Values allowed from 1 to 99."
-		echo "How to use: ./audit_ldap_log.sh minutes"
-		echo "where minutes is logfile's data was last modified minutes ago (default is 43200 minutes)"
-		exit 1
-	fi
-fi
-
-# Calcula a data retroativa
-DATE_AFTER=$(date --date="$MMIN min ago" '+%d/%m/%Y %H:%M:%S')
-
-
-# Enables installation logging
-AUDIT_LOG_FILE="audit_ldap_log_$(date '+%d%m%Y_%H%M%S').log"
-exec > >(tee -a ${LOG_FILE} )
-exec 2> >(tee -a ${LOG_FILE} >&2)
-
-# tmpfiles go to /$TMP_DIR
-mkdir -p $TMP_DIR && cd $TMP_DIR
-
-
-# Log destination
-LOG_DEST=/var/log/ems-bus
-LOG_FILE_TMP="$TMP_DIR/full_log_file.log"
-LOG_FILE="$TMP_DIR/full_log_file_filtered.log"
-
-
-# Configurações SMTP para envio de alertas aos admins
-SMTP_SERVER="mail.unb.br"
-SMTP_PORT=587
-SMTP_DE="evertonagilar@unb.br"
-SMTP_PARA="evertonagilar@unb.br,evertonagilar@unb.br"
-SMTP_PASSWD=unb9601
-
-
-# Percentual de uso da pasta de backup
-USO_LOG_DEST=$(df -h  $LOG_DEST | sed '1d' | awk '{print $5}' | sed 's/%//')
-
-
-# Quando o espaço de armazenamento $LOG_DEST chegar a este limite, um e-mail será 
-# enviado aos admins para avisar que o espaço em disco está acabando ( valor é percentual mas não inclua o '%' )
-USO_LOG_DEST_THRESHOLD=98
-
-
-
 
 # Função para enviar e-mail
 # Parâmetros:
@@ -124,45 +69,97 @@ except Exception:
 EOF
 }
 
+	
+bash -i <<EOF 
 
-# Função para realizar auditoria de logs
-audit(){
+
+echo "Starting ERLANGMS audit_ldap_log tool   ( Version: $VERSION )"
+
+
+# Check parameters
+if [ "$#" = "1" ] && [ "$1" = "--help" ]; then
+	echo "How to use: ./audit_ldap_log.sh minutes"
+	echo "where minutes is logfile's data was last modified minutes ago (default is 43200 minutes)"
+	exit 1
+elif [ "$#" = "1" ]; then
+	MMIN="$1"
+	RE='^[0-9]{1,4}$'
+	if ! [[ $MMIN =~ $RE ]] ; then
+		echo "Parameter minutes with value \"$MMIN\" is inválid. Values allowed from 1 to 99."
+		echo "How to use: ./audit_ldap_log.sh minutes"
+		echo "where minutes is logfile's data was last modified minutes ago (default is 43200 minutes)"
+		exit 1
+	fi
+fi
+
+# Calcula a data retroativa
+DATE_AFTER=$(date --date="$MMIN min ago" '+%d/%m/%Y %H:%M:%S')
+
+
+# Enables installation logging
+AUDIT_LOG_FILE="$TMP_DIR/audit_ldap_log_$(date '+%d%m%Y_%H%M%S').log"
+exec > >(tee -a ${AUDIT_LOG_FILE} )
+exec 2> >(tee -a ${AUDIT_LOG_FILE} >&2)
+
+# tmpfiles go to /$TMP_DIR
+mkdir -p $TMP_DIR && cd $TMP_DIR
+
+
+# Log destination
+LOG_DEST=/var/log/ems-bus
+LOG_FILE_TMP="$TMP_DIR/full_log_file.log"
+LOG_FILE="$TMP_DIR/full_log_file_filtered.log"
+
+
+# Configurações SMTP para envio de alertas aos admins
+SMTP_SERVER="mail.unb.br"
+SMTP_PORT=587
+SMTP_DE="evertonagilar@unb.br"
+SMTP_PARA="evertonagilar@unb.br,evertonagilar@unb.br"
+SMTP_PASSWD=unb9601
+
+
+# Percentual de uso da pasta de backup
+USO_LOG_DEST=$(df -h  $LOG_DEST | sed '1d' | awk '{print $5}' | sed 's/%//')
+
+	
 	echo "Date: $CURRENT_DATE"
 	echo "Log dest: $LOG_DEST"
 	echo "Space available for logfiles: $(( 100 - $USO_LOG_DEST ))%"
 	echo "Server: $LDAP_SERVER"
 	echo "Environment: $ENVIRONMENT"
-	echo "Analyzing the ldap log files from the date $DATE_AFTER ($MMIN minutes ago)..."
 
-
-	echo "Reading the following logfiles:"
+	echo
+	if [ "$MMIN" == "1440" ]; then
+		echo "Analyzing the ldap logs between $DATE_AFTER (1 day ago) and $CURRENT_DATE"
+	else
+		echo "Analyzing the ldap logs between $DATE_AFTER ($MMIN minutes ago) and $CURRENT_DATE"
+	fi
 	LOG_FILE_LIST=$(find "$LOG_DEST" -type f -mmin "-$MMIN" -follow)
 	if [ -z "$LOG_FILE_LIST" ]; then
 		echo "No logfiles to analyze."
 		exit 1
 	else
+		echo "Reading the following logfiles:"
 		find "$LOG_DEST" -type f -mmin "-$MMIN" -follow
 	fi 
 
 
 	# concat all logfiles (remove characters control of the line colors)
-	find "$LOG_DEST" -type f -mmin "-$MMIN" -follow | xargs sed -r 's/\0x033//' | sed -r 's/\\033\[0m\$//'  > $LOG_FILE_TMP
+	find "$LOG_DEST" -type f -mmin "-$MMIN" -follow | xargs sed -r '/ERROR/ s/.{7}//' | sed -r '/ERROR/ s/.{4}$//' > $LOG_FILE_TMP
 	
-	sed -n 'l' $LOG_FILE_TMP
-	
-	exit
 	
 
-	# filter only the log lines of the defined interval	
+	# filter only the log lines of the defined interval	$DATE_AFTER
 	awk '{
 			DT_LOG=$2" "$3;
-			if (DT_LOG > "15/02/2017 15:00:00"){
+			if (DT_LOG > "$DATE_AFTER"){
 				print; 
 			}
 	}' $LOG_FILE_TMP > $LOG_FILE
 	
 
-	
+
 	# Print title
 	echo
 	echo
@@ -203,11 +200,11 @@ audit(){
 	echo
 	echo
 
-	echo "1) Total number of success ldap requests: $REQ_SEARCH_SUCCESS_TOTAL requests"
+	echo "1) Number of success ldap requests: $REQ_SEARCH_SUCCESS_TOTAL requests"
 	echo
 	echo
 	
-	echo "2) Which users were found in the request search operation: $REQ_SEARCH_SUCCESS_UNIQ_TOTAL users"
+	echo "2) Which users were found in the ldap request search operation: $REQ_SEARCH_SUCCESS_UNIQ_TOTAL users"
 	echo '+-----------------------------------------------+------------------------------------------------------+'
 	printf "| %-45s |  %-51s |\n"  "LOGIN"   "NAME"
 	echo '+-----------------------------------------------+------------------------------------------------------+'
@@ -218,17 +215,17 @@ audit(){
 	echo
 	echo
 	
-	echo "3) Number of success requests search operation per user:"
+	echo "3) Number of success ldap requests search operation per user:"
 	echo '+-----------------------------------------------+------------------------------------------------------+'
-	printf "| %-45s |  %-51s |\n"  "LOGIN"   "REQUESTS"
+	printf "| %-45s | %-52s |\n"  "LOGIN"   "REQUESTS"
 	echo '+-----------------------------------------------+------------------------------------------------------+'
 	awk '{print $1}' request_search_success_uniq.tmp > request_search_success_uniq_login.tmp 
 	for USER in `cat request_search_success_uniq_login.tmp`; do
 		REQ_SEARCH_SUCCESS_TOTAL_By_USER=$(grep -w $USER request_search_success.tmp | wc -l | cut -d" " -f1)
-		printf "| %-45s | %17d  %34s|\n"   $USER   $REQ_SEARCH_SUCCESS_TOTAL_By_USER " "
+		printf "| %-45s | %19d  %32s|\n"   $USER   $REQ_SEARCH_SUCCESS_TOTAL_By_USER " "
 	done
 	echo '+-----------------------------------------------+------------------------------------------------------+'
-	printf "| %-45s | TOTAL:      %-24d %16s|\n"   " " $REQ_SEARCH_SUCCESS_TOTAL " "
+	printf "| %-45s | TOTAL: %12d %33s|\n"   " " $REQ_SEARCH_SUCCESS_TOTAL " "
 	echo '+-----------------------------------------------+------------------------------------------------------+'
 	
 	echo
@@ -241,10 +238,6 @@ audit(){
 
 	# create a tmp file with request search fail lines (remove caracteres de controle da cor antes de ERROR)
 	egrep '^ERROR.*Ldap' $LOG_FILE > error_request_search.tmp
-	
-	cat $LOG_FILE
-	cat error_request_search.tmp
-	
 	REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_TOTAL=$(wc -l error_request_search.tmp | cut -d" " -f1)
 	
 	# create a tmp file with request search login does not exist lines
@@ -263,61 +256,37 @@ audit(){
 	echo
 	echo
 	
-	echo "1) Number of failed request attempts because login does not exist: $REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_TOTAL requests"
+	echo "1) Number of failed ldap request attempts because login does not exist: $REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_TOTAL requests"
 	echo
 	echo
 	
-	echo "2) Which users were not found in the request search operation: $REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_UNIQ_TOTAL users"
+	echo "2) Which users were not found in the ldap request search operation: $REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_UNIQ_TOTAL users"
 	echo '+-----------------------------------------------+------------------------------------------------------+'
-	printf "| %-45s |  %-51s |\n"  "LOGIN"   "REQUESTS"                                                                  
+	printf "| %-45s | %-52s |\n"  "LOGIN"   "REQUESTS"                                                                  
 	echo '+-----------------------------------------------+------------------------------------------------------+'
 	for USER in `cat request_search_login_does_not_exist_uniq.tmp`; do
 		REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_TOTAL_BY_USER=$(grep -w $USER request_search_login_does_not_exist.tmp | wc -l | cut -d" " -f1)
-		printf "| %-45s | %18d         %26s|\n"   $USER   $REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_TOTAL_BY_USER " "
+		printf "| %-45s | %19d         %25s|\n"   $USER   $REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_TOTAL_BY_USER " "
 	done
 	echo '+-----------------------------------------------+------------------------------------------------------+'
-	printf "| %-45s | TOTAL:      %-24d %16s|\n"   " " $REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_TOTAL " "
+	printf "| %-45s | TOTAL: %12d %33s|\n"   " " $REQ_SEARCH_FAIL_LOGIN_DOES_NOT_EXIST_TOTAL " "
 	echo '+-----------------------------------------------+------------------------------------------------------+'
 	
 	echo
 	echo
 
-
+EOF
 	
+	TextLog=$(cat $AUDIT_LOG_FILE)
+	envia_email "Relatório de auditoria dos logs LDAP do barramento ERLANGMS" \
+				"$TextLog"  
 	
-}
-
-
-# Função para alertar um backup bem sucedido
-alerta_sucesso_backup(){
-	TextLog=$(cat $LOG_FILE)
-	envia_email "Alerta de backup do GitLab bem sucedido." \
-				"Log da operação do backup:\n\n$TextLog\n\nAtt.\n\t git-backup.sh"  
-}
-
-
-# Função para alertar um backup mal sucedido
-alerta_falha_backup(){
-	TextLog=$(cat $LOG_FILE)
-	envia_email "Alerta de backup do GitLab não realizado!!!" \
-				"Atenção: Ocorreu um erro ao realizar o backup do GitLab..\n\nLog da tentativa de backup:\n\n$TextLog\n\nAtt.\n\t git-backup.sh"  
-}
-
-
-# Função para verificar e alertar se a pasta de backup está ficando sem espaço
-verifica_espaco_disponivel(){
-	if [ "$USO_LOG_DEST" -ge "$USO_LOG_DEST_THRESHOLD" ]; then
-		TextLog=$(cat $LOG_FILE)
-		envia_email "Alerta de falta de espaço para backup do GitLab" \
-		            "Atenção: O espaço da unidade de backup do GitLab está em $USO_LOG_DEST% da capacidade!!!\n\nLog da última operação de backup:\n\n$TextLog\n\nAtt.\n\t git-backup.sh"  
-	fi
-}
+	echo "concluído."
+	
 
 
 
-############## main ####################
 
-audit
 cd $CURRENT_DIR
 
 
