32892db60120eccd5c8a2ea847df97ab4acb2ead
evertonagilar <evertonagilar@gmail.com>
2017-03-21 14:00:14 -0300

    pequenos ajustes

--- src/user/ems_user_loader.erl
+++ src/user/ems_user_loader.erl
@@ -18,11 +18,11 @@
 
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/1, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/1, handle_info/2, terminate/2, code_change/3, 
+		 last_update/0, is_empty/0, size_table/0, force_load_users/0]).
 
 % estado do servidor
 -record(state, {datasource,
-				operation,
 				update_checkpoint,
 				last_update}).
 
@@ -39,6 +39,15 @@ stop() ->
     gen_server:cast(?SERVER, shutdown).
  
 
+last_update() -> ems_db:get_param(<<"ems_user_loader_lastupdate">>).
+	
+is_empty() -> mnesia:table_info(user, size) == 0.
+
+size_table() -> mnesia:table_info(user, size).
+
+force_load_users() -> 
+	gen_server:cast(?SERVER, force_load_users),
+	ok.
 
  
 %%====================================================================
@@ -49,58 +58,57 @@ init(#service{datasource = Datasource,
 			  properties = Props}) ->
 	LastUpdate = ems_db:get_param(<<"ems_user_loader_lastupdate">>),
 	UpdateCheckpoint = maps:get(<<"update_checkpoint">>, Props, ?USER_LOADER_UPDATE_CHECKPOINT),
+	set_force_load_users_checkpoint(),
 	State = #state{datasource = Datasource, 
-				   operation = load_or_update_operation(),
 				   update_checkpoint = UpdateCheckpoint,
 				   last_update = LastUpdate},
-	{ok, State, 3000}.
+	{ok, State, 2000}.
     
 handle_cast(shutdown, State) ->
     {stop, normal, State};
 
-handle_cast(_Msg, State) ->
-	{noreply, State}.
+handle_cast(force_load_users, State = #state{update_checkpoint = UpdateCheckpoint}) ->
+	ems_logger:warn("ems_user_loader force load the users now."),
+	State2 = State#state{last_update = undefined},
+	update_or_load_users(State2),
+	{noreply, State, UpdateCheckpoint};
+
+handle_cast(_Msg, State = #state{update_checkpoint = UpdateCheckpoint}) ->
+	{noreply, State, UpdateCheckpoint}.
 
 handle_call(Msg, _From, State) ->
 	{reply, Msg, State}.
 
-handle_info(State) ->
-   {noreply, State, 1000}.
+handle_info(State = #state{update_checkpoint = UpdateCheckpoint}) ->
+   {noreply, State, UpdateCheckpoint}.
 
-handle_info(timeout, State = #state{datasource = Datasource,
-									operation = update_users,
-									update_checkpoint = UpdateCheckpoint,
-									last_update = LastUpdate}) ->
-	?DEBUG("ems_user_loader checkpoint. operation: update_users."),
-	NextUpdate = calendar:local_time(),
-	case update_users_from_datasource(Datasource, LastUpdate, ems_util:timestamp_str()) of
-		ok -> 
-			ems_db:set_param(<<"ems_user_loader_lastupdate">>, NextUpdate),
-			State2 = State#state{last_update = NextUpdate, 
-								  operation = load_or_update_operation()},
-			{noreply, State2, UpdateCheckpoint};
+handle_info(check_force_load_users, State = #state{update_checkpoint = UpdateCheckpoint}) ->
+	{{_, _, _}, {Hour, _, _}} = calendar:local_time(),
+	case Hour >= 4 andalso Hour =< 6 of
+		true ->
+			ems_logger:info("ems_user_loader force load users checkpoint."),
+			State2 = State#state{last_update = undefined},
+			case update_or_load_users(State2) of
+				{ok, State3} ->
+					erlang:send_after(86400 * 1000, self(), check_force_load_users),
+					{noreply, State3, UpdateCheckpoint};
+				{error, State3} -> 
+					erlang:send_after(60000 * 5, self(), check_force_load_users),
+					{noreply, State3, UpdateCheckpoint}
+			end;
 		_ -> 
+			set_force_load_users_checkpoint(),
 			{noreply, State, UpdateCheckpoint}
 	end;
-		
 
-handle_info(timeout, State = #state{datasource = Datasource,
-									operation = load_users,
-									update_checkpoint = UpdateCheckpoint}) ->
-	?DEBUG("ems_user_loader checkpoint. operation: load_users."),
-	NextUpdate = calendar:local_time(),
-	case load_users_from_datasource(Datasource, ems_util:timestamp_str()) of
-		ok -> 
-			ems_db:set_param(<<"ems_user_loader_lastupdate">>, NextUpdate),
-			State2 = State#state{operation = load_or_update_operation(),
-								 last_update = NextUpdate},
-			{noreply, State2, UpdateCheckpoint};
-		_ -> 
-			{noreply, State, UpdateCheckpoint}
+handle_info(timeout, State = #state{update_checkpoint = UpdateCheckpoint}) ->
+	case update_or_load_users(State) of
+		{ok, State2} ->	{noreply, State2, UpdateCheckpoint};
+		{error, State2} -> {noreply, State2, UpdateCheckpoint}
 	end;
 	
 handle_info({_Pid, {error, Reason}}, State = #state{update_checkpoint = UpdateCheckpoint}) ->
-	ems_logger:warn("ems_user_loader is unable to update users. Reason: ~p.", [Reason]),
+	ems_logger:warn("ems_user_loader is unable to load or update users. Reason: ~p.", [Reason]),
 	{noreply, State, UpdateCheckpoint}.
 			
 terminate(_Reason, _State) ->
@@ -110,15 +118,39 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 	
+
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
--spec load_or_update_operation() -> load_users | update_users.
-load_or_update_operation() ->
-	case mnesia:table_info(user, size) of
-		 0 -> load_users;
-		 _ -> update_users
+set_force_load_users_checkpoint() ->
+	erlang:send_after(60000 * 60, self(), check_force_load_users).
+
+update_or_load_users(State = #state{datasource = Datasource,
+									last_update = LastUpdate}) ->
+	NextUpdate = calendar:local_time(),
+	TimestampStr = ems_util:timestamp_str(),
+	case is_empty() orelse LastUpdate == undefined of
+		true -> 
+			ems_logger:info("ems_user_loader checkpoint. operation: load_users."),
+			case load_users_from_datasource(Datasource, TimestampStr) of
+				ok -> 
+					ems_db:set_param(<<"ems_user_loader_lastupdate">>, NextUpdate),
+					State2 = State#state{last_update = NextUpdate},
+					{ok, State2};
+				_ -> 
+					{error, State}
+			end;
+		false ->
+			?DEBUG("ems_user_loader checkpoint. operation: update_users   last_upate: ~s.", [ems_util:timestamp_str(LastUpdate)]),
+			case update_users_from_datasource(Datasource, LastUpdate, TimestampStr) of
+				ok -> 
+					ems_db:set_param(<<"ems_user_loader_lastupdate">>, NextUpdate),
+					State2 = State#state{last_update = NextUpdate},
+					{ok, State2};
+				_ -> 
+					{error, State}
+			end
 	end.
 
 
@@ -126,7 +158,7 @@ load_users_from_datasource(Datasource, CtrlInsert) ->
 	try
 		case ems_odbc_pool:get_connection(Datasource) of
 			{ok, Datasource2} -> 
-				?DEBUG("ems_user_loader got a connection to load users."),
+				ems_logger:info("ems_user_loader load users from database..."),
 				Result = case ems_odbc_pool:param_query(Datasource2, 
 														sql_load_users(), 
 														[], 
@@ -135,13 +167,19 @@ load_users_from_datasource(Datasource, CtrlInsert) ->
 						?DEBUG("ems_user_loader did not load any users."),
 						ok;
 					{_, _, Records} ->
-						F = fun() ->
-							Count = insert_users(Records, 0, CtrlInsert),
-							ems_logger:info("ems_user_loader load ~p users.", [Count])
-						end,
-						mnesia:ets(F),
-						mnesia:change_table_copy_type(user, node(), disc_copies),
-						ok;
+						case mnesia:clear_table(user) of
+							{atomic, ok} ->
+								F = fun() ->
+									Count = insert_users(Records, 0, CtrlInsert),
+									ems_logger:info("ems_user_loader load ~p users.", [Count])
+								end,
+								mnesia:ets(F),
+								mnesia:change_table_copy_type(user, node(), disc_copies),
+								ok;
+							_ ->
+								ems_logger:error("Could not clear user table before load users. Load users cancelled!"),
+								{error, efail_load_users}
+						end;
 					{error, Reason} = Error -> 
 						ems_logger:error("ems_user_loader load users error: ~p.", [Reason]),
 						Error
@@ -163,22 +201,16 @@ update_users_from_datasource(Datasource, LastUpdate, CtrlUpdate) ->
 		case ems_odbc_pool:get_connection(Datasource) of
 			{ok, Datasource2} -> 
 				?DEBUG("ems_user_loader got a connection ~p to update users.", [Datasource#service_datasource.id]),
-				case LastUpdate of 
-					undefined -> 
-						Sql = sql_load_users(),
-						Params = [];
-					_ -> 
-						Sql = sql_update_users(),
-						{{Year, Month, Day}, {Hour, Min, _}} = LastUpdate,
-						% Zera os segundos para trazer todos os registros alterados no intervalor de 1 min
-						DateInitial = {{Year, Month, Day}, {Hour, Min, 0}},
-						Params = [{sql_timestamp, [DateInitial]},
-								  {sql_timestamp, [DateInitial]},
-								  {sql_timestamp, [DateInitial]},
-								  {sql_timestamp, [DateInitial]},
-								  {sql_timestamp, [DateInitial]},
-								  {sql_timestamp, [DateInitial]}]
-				end, 
+				Sql = sql_update_users(),
+				{{Year, Month, Day}, {Hour, Min, _}} = LastUpdate,
+				% Zera os segundos para trazer todos os registros alterados no intervalor de 1 min
+				DateInitial = {{Year, Month, Day}, {Hour, Min, 0}},
+				Params = [{sql_timestamp, [DateInitial]},
+						  {sql_timestamp, [DateInitial]},
+						  {sql_timestamp, [DateInitial]},
+						  {sql_timestamp, [DateInitial]},
+						  {sql_timestamp, [DateInitial]},
+						  {sql_timestamp, [DateInitial]}],
 				Result = case ems_odbc_pool:param_query(Datasource2, Sql, Params, ?MAX_TIME_ODBC_QUERY) of
 					{_,_,[]} -> 
 						?DEBUG("ems_user_loader did not update any users."),
@@ -187,7 +219,7 @@ update_users_from_datasource(Datasource, LastUpdate, CtrlUpdate) ->
 						%?DEBUG("Update users ~p.", [Records]),
 						F = fun() ->
 							Count = update_users(Records, 0, CtrlUpdate),
-							ems_logger:info("ems_user_loader update ~p users.", [Count])
+							ems_logger:info("ems_user_loader update ~p users since ~s.", [Count, ems_util:timestamp_str(LastUpdate)])
 						end,
 						mnesia:activity(transaction, F),
 						ok;
