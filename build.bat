df0e4d4d55754c0da485f95a51b0c76c0f3845c0
Renato Carauta Ribeiro <rcarauta@unb.br>
2017-06-08 10:58:32 -0300

    Comando para remover bibliotecas e baixa-las novamente ao executar o build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @echo off
 cls
 echo Build ems-bus with rebar...
+rmdir  /S /Q deps\cowboy
+rmdir  /S /Q deps\cowlib
+rmdir  /S /Q deps\ranch
+rmdir  /S /Q deps\oauth2
 if not exist priv\log goto DeleteDBFolder
 rmdir /S /Q priv\log
 :DeleteDBFolder
